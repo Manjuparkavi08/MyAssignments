b7461399f056622897014537276bfc17344f2322
Manjuparkavi08 <102411188+Manjuparkavi08@users.noreply.github.com>
2025-09-25 13:21:56 +0530

    Add files via upload

---
+++ Playwright Class Hw/PW_StringRev_Palin_HW6.js
@@ -0,0 +1,50 @@
+let strvalue= "MADAM";
+console.log("The Given String Value is: ", strvalue);
+let splitvalue = strvalue.split("");
+console.log("Split the Given string into each characters as: ", splitvalue);
+
+//let strIndex = strvalue.indexOf("A");
+//console.log("The Index Value of the Charcter is:", strIndex);
+//let strInedxChar = strvalue.charAt(0,4);
+//console.log("The Char Value of the Index is:", strInedxChar);
+//let strlen = strvalue.length;
+//console.log("The Length of the given string is:", strlen);
+
+let strRev = "";
+for (let index = strvalue.length-1; index >=0; index--  ) {
+   // console.log(index); //0 1 2 3 4 (in next lines)
+   // console.log(strvalue[index]); M A D A M (in next lines)
+    strRev = strRev.concat(strvalue[index]);
+}
+console.log("The Reverse Of the Given String is:", strRev);
+
+if (strvalue===strRev) {
+    console.log("The Given String is a Palindrome");
+    
+    
+} else {
+    console.log("The Given String is NOT a Palindrome");
+        
+}
+
+
+    
+    
+
+
+/* for (let index = strlen-1; index >= 0; index--) {
+strRev += splitvalue[index];
+}
+console.log("The Reverse of the Given String is: ", strRev);
+
+console.log("The Actual String is:", strvalue);
+console.log("The Reversed String is:", strRev);
+
+
+if (strvalue===strRev) {
+    console.log("The Given String Value is a Palindrome");
+    
+} else {
+    console.log("The Given String Value is NOT a Palindrome");
+    
+} */
