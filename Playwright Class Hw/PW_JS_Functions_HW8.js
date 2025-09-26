97b251b5e554c0eb63c48e53c44e6533fe00cc21
Manjuparkavi08 <102411188+Manjuparkavi08@users.noreply.github.com>
2025-09-26 20:40:27 +0530

    Add files via upload

---
+++ Playwright Class Hw/PW_JS_Functions_HW8.js
@@ -0,0 +1,33 @@
+//  Task 1 - Function Declaration
+function userProfile(name) {
+    console.log("1. This is an Example of Named Function:");
+    console.log("Hello ".concat(name,"!", '\n'));
+    
+} userProfile("Test Leaf")
+
+//  Task 2 - Arrow Function
+let double = (x) => { // fat arrow
+    console.log("2. This is an Example of Arrow Function:");
+console.log("The doubled value is: ", x*x, '\n');
+}
+double(5)
+    
+ // Task 3 - Anonymous Function
+let Timeout = setTimeout (function () {
+    console.log("3. This is an Example of Anonymous Function:");
+    console.log("This Message is Delayed by 2 seconds", '\n');
+}, 1000);  
+
+// Task 4 - Call Back Function
+function getUserData(callBackFunction) {
+    setTimeout(function () {
+       console.log("4. This is an Example of Callback Function:");
+       callBackFunction ()
+      }  ,3000);
+     }
+    
+    function callBackFunction () {
+        console.log("This Message is Printed after 3 seconds");
+        
+    }
+ getUserData( callBackFunction)
