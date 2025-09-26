1e5aae2a9c017fa81a9d4a851198a961d5e0a43b
Manjuparkavi08 <102411188+Manjuparkavi08@users.noreply.github.com>
2025-09-26 23:52:17 +0530

    Add files via upload

---
+++ Playwright Class Hw/PW_Arr_Intersection_HW10.js
@@ -0,0 +1,22 @@
+// No Common Elements
+function intersection() {
+    let array1 = [1,2,3,4,5];
+    let array2 = [6,7,8,9,10,11];
+    for (let index = 0; index < array1.length; index++) {
+        //const element1 = array[index];
+        
+    for (let jdex = 0; jdex < array2.length; jdex++) {
+        //const element2 = array[jdex];
+         if(array1[index]!=array2[jdex]) {
+    } 
+    {
+}
+               {
+                
+                
+        }console.log(array1.push(array2))
+  
+         }
+     
+    
+        } intersection()
