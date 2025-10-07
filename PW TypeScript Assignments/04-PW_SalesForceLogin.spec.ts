9b95c669adb9ccf2eaa3d3e96243d41962b13a4f
Manjuparkavi08 <102411188+Manjuparkavi08@users.noreply.github.com>
2025-10-07 23:15:41 +0530

    Update 04-PW_SalesForceLogin.spec.ts

--- PW TypeScript Assignments/04-PW_SalesForceLogin.spec.ts
+++ PW TypeScript Assignments/04-PW_SalesForceLogin.spec.ts
@@ -3,6 +3,7 @@ import {test} from "@playwright/test";
 test ("To Login to Salesforce Login", async ({page}) => {
     await page.goto("https://login.salesforce.com/");
     await page.locator(`//input[@id="username"]`).fill(`manjuparkavi46187@agentforce.com`);
-    await page.locator(`//input[@id="password"]`).fill(`SaiAgastya@21`);
+    await page.locator(`//input[@id="password"]`).fill(`Satya@21`);
     await page.locator(`//input[@id="Login"]`).click();
+
 })
